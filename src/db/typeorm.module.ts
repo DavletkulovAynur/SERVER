83eaf8ba11a60fd0f768680e888a7b034b034043
afdavletkulov <afdavletkulov@rttv.ru>
2024-02-16 17:29:31 -0300

    fix:

--- src/db/typeorm.module.ts
+++ src/db/typeorm.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule as NestTypeOrmModule } from '@nestjs/typeorm';
 
-console.log(process.env.POSTGRES_DATABASE);
+console.log(process.env);
+
+const isDev = false;
 @Module({
   imports: [
     NestTypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST, //localhost
-      port: Number(process.env.POSTGRES_PORT), //5432
-      username: process.env.POSTGRES_USERNAME, //test ??
-      password: process.env.POSTGRES_PASSWORD, //test ??
-      database: process.env.POSTGRES_DATABASE, //accountant ??
-      entities: ['dist/entities/**/*.entity.js'],
-      synchronize: true,
-      // migrations: [ 'dist/db/migrations/**/*.js' ],
-      // cli: { migrationsDir: 'src/db/migrations' },
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: isDev ? 'default_db' : 'default_db', //Переключить
+      autoLoadEntities: true,
+      synchronize: true, //В production нужно использовать использовать миграцию
+      // synchronize: isDev,
     }),
   ],
 })
